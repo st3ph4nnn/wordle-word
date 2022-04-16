ff9a581e32b50b72914f4f042d6b60866f90f07b
Stefan Arteni <stephanarteni@gmail.com>
2022-04-16 12:09:15 -0700

    first release

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var colors = ['#325ca8', '#3832a8', '#5732a8', '#7532a8'];
 var random_color = colors[Math.floor(Math.random() * colors.length)];
 
 const d = new Date();
-document.getElementById('today').innerHTML = d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear() + " wordle word is..."
+document.getElementById('today').innerHTML = d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear() + " wordle word is"
 document.getElementById('word').style.color = random_color;
 document.getElementById('word').innerHTML = get_word();
